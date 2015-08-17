d6f037e03ace6b87c6d301b7ccdfe84114ab3eee
Todd Laney <ToddLa@Todds-MacBook.T-mobile.com>
2015-08-17 11:45:13 -0700

    Wombat

--- smashtagpd/TweetTableViewController.swift
+++ smashtagpd/TweetTableViewController.swift
@@ -18,18 +18,18 @@ class TweetTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let request = TwitterRequest(search: "#stanford", count: 100 )
-        
-        request.fetchTweets { (newTweets) -> Void in
-            dispatch_async((dispatch_get_main_queue())
-                { () -> Void in
-                    if newTweets.count > 0
-                    {
-                        self.tweets.insert(newTweets, atIndex: 0)
-                        self.tableView.reloadData()
-                    }
+        let request = TwitterRequest(search:"#stanford", count:100)
+
+        request.fetchTweets {newTweets in
+
+            dispatch_async(dispatch_get_main_queue()) {
+                if newTweets.count > 0
+                {
+                    self.tweets.insert(newTweets, atIndex: 0)
+                    self.tableView.reloadData()
                 }
             }
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
